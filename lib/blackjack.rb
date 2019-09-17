f87b76609b3ff3d5428507fe681727f267d202b2
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-09-17 11:24:11 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,50 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  number = rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(hand)
+  puts "Your cards add up to #{hand}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(hand)
+  puts "Sorry, you hit #{hand}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  total = 0
+  total += deal_card
+  total += deal_card
+  display_card_total(total)
+  total
 end
 
-def hit?
-  # code hit? here
+def hit?(hand)
+  prompt_user
+  answer = get_user_input
+  if answer == 'h'
+    hand += deal_card
+  elsif answer != 's'
+    invalid_command
+    prompt_user
+    get_user_input
+  end
+  hand
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +52,18 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  handz = initial_round
+  until handz > 21 do
+    new_hand = hit?(handz)
+    if new_hand == handz
+      puts "Are you sure you want to stay?"
+      prompt_user
+      break if get_user_input == 's'
+    end
+    handz = new_hand
+    display_card_total(new_hand)
+  end
+  end_game(handz)
 end
     
